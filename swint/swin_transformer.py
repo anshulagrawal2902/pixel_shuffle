c98431a5c0ffb689ae78c876d62fe5e4c3810268
Anshul Agrawal <anshulagrawal2902@gmail.com>
2024-04-11 08:05:52 +0530

    no self.shift_size transformer block

--- swint/swin_transformer.py
+++ swint/swin_transformer.py
@@ -269,9 +269,9 @@ class SwinTransformerBlock(nn.Module):
 
         return x
 
-    def extra_repr(self) -> str:
-        return f"dim={self.dim}, input_resolution={self.input_resolution}, num_heads={self.num_heads}, " \
-               f"window_size={self.window_size}, shift_size={self.shift_size}, mlp_ratio={self.mlp_ratio}"
+    # def extra_repr(self) -> str:
+    #     return f"dim={self.dim}, input_resolution={self.input_resolution}, num_heads={self.num_heads}, " \
+    #            f"window_size={self.window_size}, shift_size={self.shift_size}, mlp_ratio={self.mlp_ratio}"
 
     def flops(self):
         flops = 0
